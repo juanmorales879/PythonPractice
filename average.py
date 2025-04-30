2d137fc2c0d39f16747eee6d49e865273d3cb528
juanmorales879 <juanmorales.marcano@gmail.com>
2025-04-29 18:54:10 -0700

    Add files via upload

---
+++ average.py
@@ -0,0 +1,7 @@
+def find_average(numbers):
+    if len(numbers) == 0:
+        return False
+    return sum(numbers) / len(numbers)
+
+def find_average(numbers):
+    return sum(numbers) / len(numbers) if numbers else 0
